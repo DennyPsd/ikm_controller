b35f3911a588f7f51e6ef8eb9fd8fa109b34821c
exdet <egor-nic2001@mail.ru>
2025-12-05 13:26:25 +0300

    WIP

--- src/types/parks.rs
+++ src/types/parks.rs
@@ -1,6 +1,6 @@
 use serde::{Deserialize, Serialize};
 #[derive(Debug, Clone, Serialize, Deserialize)]
-pub struct ParkDocument {
+pub struct Park {
   #[serde(rename = "_id", skip_serializing_if = "Option::is_none")]
   pub id: Option<String>,
   pub name: String,
