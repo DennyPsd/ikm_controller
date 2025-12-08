52336e6a016b5932ac2790c8d40898cee6ae7496
korotkov.nf <korotkov.nf@gmail.com>
2025-12-09 01:29:36 +0300

    update

--- src/types/tank_configuration.rs
+++ src/types/tank_configuration.rs
@@ -1,75 +1,123 @@
 use schemars::JsonSchema;
 use serde::{Deserialize, Serialize};
 
+/** Конфигурация цистерны */
 #[derive(Default, Deserialize, Serialize, Debug, Clone, JsonSchema, PartialEq)]
 pub struct TankConfig {
+  /** Основные данные */
   pub basic_data: TankBasicData,
+  /** Уровни точечных датчиков */
   pub levels_of_point_sensors: LevelsOfPointSensors,
+  /** Конструкция */
   pub construction: Construction,
+  /** Метод расчета массы */
   pub mass_calculation_method: MassCalculationMethod,
+  /** Показатели точности измерений */
   pub measurement_accuracy_indicators: MeasurementAccuracyIndicators,
+  /** Блок калибровки */
   pub calibration_block: Calibration,
 }
 
+/** Основные данные цистерны */
 #[derive(Default, Deserialize, Serialize, Debug, Clone, JsonSchema, PartialEq)]
 pub struct TankBasicData {
+  /** Наименование */
   pub name: Option<String>,
+  /** Парк резервуаров */
   pub reservoir_park: Option<String>,
+  /** Группа */
   pub group: Option<String>,
+  /** Тип цистерны */
   pub tank_type: Option<String>,
+  /** Номинальная емкость */
   pub nominal_capacity: Option<String>,
+  /** Плотность хранимой жидкости по данным */
   pub density_stored_liquid_according: Option<String>,
+  /** Базовая высота */
   pub basic_height: Option<String>,
+  /** Продукт */
   pub product: Option<String>,
+  /** Максимальный допустимый уровень продукта */
   pub maximum_allowable_product_level: Option<String>,
 }
 
+/** Точечный датчик уровня */
 #[derive(Default, Deserialize, Serialize, Debug, Clone, JsonSchema, PartialEq)]
 pub struct LevelPointSensor {
+  /** Идентификатор */
   pub id: String,
+  /** Индекс */
   pub index: i32,
+  /** Значение */
   pub value: u32,
 }
 
+/** Уровни точечных датчиков */
 #[derive(Default, Deserialize, Serialize, Debug, Clone, JsonSchema, PartialEq)]
 pub struct LevelsOfPointSensors {
+  /** Гистерезис */
   pub hysteresis: Option<String>,
+  /** Точки датчиков */
   #[serde(default)]
   pub points: Vec<LevelPointSensor>,
 }
 
+/** Конструкция цистерны */
 #[derive(Default, Deserialize, Serialize, Debug, Clone, JsonSchema, PartialEq)]
 pub struct Construction {
+  /** Линейное расширение */
   pub linear_expansion: Option<String>,
+  /** Масса плавающего покрытия */
   pub mass_floating_coating: Option<String>,
 }
 
+/** Метод расчета массы */
 #[derive(Default, Deserialize, Serialize, Debug, Clone, JsonSchema, PartialEq)]
 pub struct MassCalculationMethod {
+  /** Метод */
   pub method: Option<String>,
+  /** Уровень переключения */
   pub switching_level: Option<String>,
+  /** Гистерезис уровня переключения */
   pub switching_level_hysteresis: Option<String>,
+  /** P3-P1 */
   pub p3_p1: Option<String>,
+  /** Опорная точка P1 */
   pub p1_reference_point: Option<String>,
+  /** Опорная точка */
   pub reference_point: Option<String>,
 }
 
+/** Показатели точности измерений */
 #[derive(Default, Deserialize, Serialize, Debug, Clone, JsonSchema, PartialEq)]
 pub struct MeasurementAccuracyIndicators {
+  /** Допустимая абсолютная погрешность */
   pub limit_permissible_absolute_error: Option<String>,
+  /** Предел гидростатического устройства */
   pub hydrostatic_device_limit: Option<String>,
+  /** Предел давления устройства */
   pub pressure_device_limit: Option<String>,
+  /** Допустимая абсолютная погрешность измерения уровня резервуара */
   pub limit_permissible_absolute_measurement_reservoir_level: Option<String>,
+  /** Допустимая абсолютная погрешность измерения уровня сырой воды */
   pub limit_permissible_absolute_measurement_level_raw_water: Option<String>,
+  /** Допустимая абсолютная погрешность измерения температуры продуктов и паров */
   pub limit_permissible_absolute_measurement_temp_products_and_vapours: Option<String>,
+  /** Допустимая абсолютная погрешность измерения плотности нефти */
   pub limit_permissible_absolute_measurement_oil_densities: Option<String>,
+  /** Калибровочная таблица чертежа */
   pub drawing_calibration_table: Option<String>,
+  /** ВПИ гидростатического устройства */
   pub hydrostatic_device_vpi: Option<String>,
+  /** ВПИ давления устройства */
   pub pressure_device_vpi: Option<String>,
 }
 
+/** Калибровка */
 #[derive(Default, Deserialize, Serialize, Debug, Clone, JsonSchema, PartialEq)]
 pub struct Calibration {
+  /** Коэффициент уровня */
   pub level_coefficient: Option<String>,
+  /** Точечные датчики уровня */
   pub level_point_sensors: Vec<LevelPointSensor>,
 }
--- src/types/tanks.rs
+++ src/types/tanks.rs
@@ -3,64 +3,118 @@ use crate::types::tank_configuration::TankConfig;
 use schemars::JsonSchema;
 use serde::{Deserialize, Serialize};
 use smol_str::SmolStr;
+use taxon_core::infrastructure::facility::SharedData;
 use uuid::Uuid;
 
+/// Тип статуса цистерны
 pub type TankStatus = String;
 
+/// Базовые показатели цистерны
 #[derive(Default, Deserialize, Serialize, Debug, Clone, JsonSchema, PartialEq)]
 pub struct BaseVars {
+  /// Масса
   pub weight: f64,
+  /// Рабочий расчетный объем
   pub work_calc_vol: f64,
+  /// Средняя температура продукта
   pub product_avg_temp: f64,
+  /// Плотность продукта
   pub product_dens: f64,
 }
 
+/// Параметры отображения цистерны
 #[derive(Default, Deserialize, Serialize, Debug, Clone, JsonSchema, PartialEq)]
 pub struct TankDisplay {
+  /// Минимальный уровень цистерны
   pub min_tank_level: f64,
+  /// Максимальный уровень цистерны
   pub max_tank_level: f64,
+  /// Минимальный уровень
   pub min_level: Option<f64>,
+  /// Максимальный уровень
   pub max_level: Option<f64>,
+  /// Текущий уровень
   pub level: f64,
 }
 
+/// Цистерна
 #[derive(Default, Deserialize, Serialize, Debug, Clone, JsonSchema, PartialEq)]
 pub struct Tank {
+  /// Идентификатор
   pub id: Uuid,
+  /// Группа
   pub group: SmolStr,
+  /// Продукт
   pub product: Option<Product>,
+  /// Наименование
   pub name: SmolStr,
+  /// Статус
   pub status: TankStatus,
+  /// ГРК (группа резервуарного комплекса)
   pub grc: SmolStr,
+  /// ТРК (тип резервуарного комплекса)
   pub trc: SmolStr,
+  /// Базовые переменные
   pub base_vars: Option<BaseVars>,
+  /// Внешние переменные
   pub ext_vars: Option<ExtVars>,
+  /// Параметры отображения
   pub display_params: Option<TankDisplay>,
+  /// Конфигурация
   pub config: Option<TankConfig>,
 }
+impl SharedData for Tank {
+  fn id(&self) -> &Uuid {
+    &self.id
+  }
+  fn title(&self) -> Option<&SmolStr> {
+    Some(&self.name)
+  }
+}
+/// Расширенные показатели цистерны
 #[derive(Default, Deserialize, Serialize, Debug, Clone, JsonSchema, PartialEq)]
 pub struct ExtVars {
+  /// Объем продукта
   pub product_volume: Option<f64>,
+  /// Уровень продукта
   pub product_level: Option<f64>,
+  /// Уровень воды
   pub water_level: Option<f64>,
+  /// Температура продукта
   pub product_temperature: Option<f64>,
+  /// Температура пара
   pub vapour_temperature: Option<f64>,
+  /// Плотность продукта
   pub product_density: Option<f64>,
+  /// Плотность продукта при 15°C
   pub product_at_15_density: Option<f64>,
+  /// Гидростатическое давление
   pub hydrostatic_pressure: Option<f64>,
+  /// Давление пара
   pub vapour_pressure: Option<f64>,
+  /// Максимальная резервная емкость сверху
   pub reserve_capacity_up_max: Option<f64>,
+  /// Минимальный резервный продукт сверху
   pub reserve_product_up_min: Option<f64>,
+  /// Потребление при движении продукта
   pub product_movement_consumption: Option<f64>,
+  /// Скорость измерения уровня при движении продукта
   pub product_movement_level_measurement_speed: Option<f64>,
+  /// Объем продукта под водой
   pub volume_product_calc_below_water: Option<f64>,
+  /// Объем сырой воды
   pub volume_raw_water: Option<f64>,
+  /// Температуры
   pub temperatures: Vec<Temperature>,
 }
 
+/// Температурная точка
 #[derive(Default, Deserialize, Serialize, Debug, Clone, JsonSchema, PartialEq)]
 pub struct Temperature {
+  /// Значение температуры
   pub value: f64,
+  /// Уровень
   pub level: u32,
+  /// Наименование
   pub name: String,
 }
